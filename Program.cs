bb8a23a6bf0f4d6f12e17aa66263917881c2e826
galmodovar <galmodovar1@gmail.com>
2022-06-05 14:26:07 -0500

    Adding scoreboard

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 
-
-int score1 = 0;
-int score2 = 0;
-
+int Score1 = 0;
+int Score2 = 0;
 
 Main();
 
@@ -11,28 +9,28 @@ void Main()
 {
     Console.WriteLine("Rock Paper Scissors Game");
     Console.WriteLine("--------------------------------------------");
-    Console.WriteLine($"| Player 1: {score1} | Computer: {score2} |");
+    Console.WriteLine($"| Player 1: {Score1} | Computer: {Score2} |");
     Console.WriteLine("--------------------------------------------");
     Console.WriteLine("What would you like to throw?");
     Console.WriteLine("1) Rock");
     Console.WriteLine("2) Paper");
     Console.WriteLine("3) Scissors");
-    Console.WriteLine("Press 'enter' to exit");
+    Console.WriteLine("4) Exit");
+    
+    Game();
 
-    PlayerChoice();
-    ComputerChoice();
 }
 
-void PlayerChoice()
+int PlayerChoice()
 {
     int Selection = int.Parse(Console.ReadLine());
-    Console.WriteLine($"You Chose {Selection}");
+    return Selection;
 }
 
-void ComputerChoice()
+int ComputerChoice()
 {
     int CPUselection = GetRandomNumber();
-    Console.WriteLine($"Computer chose {CPUselection}");
+    return CPUselection;
     
     //Function to get a random number 
     static int GetRandomNumber() 
@@ -41,5 +39,60 @@ void ComputerChoice()
         int genRand= r.Next(1,4);
         return genRand;
    }
+}
+
+void Game()
+{
+    int PScore = PlayerChoice();
+    int CScore = ComputerChoice();
+    
+    //ScoreBoard(PScore, CScore);
+
+    while ( Score1 != 3 || PScore != 4)
+    {
+        if ( PScore > 0 && PScore < 4 )
+        {
+            ScoreBoard(PScore, CScore);
+            PScore = PlayerChoice();
+            CScore = ComputerChoice();
+        }
+        else if ( PScore == 4)
+        {
+            Console.WriteLine("Exiting...");
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("Make a valid selection...");
+            PScore = PlayerChoice();
+            CScore = ComputerChoice();
+            ScoreBoard(PScore, CScore);
+        }
+    }
+}
+
+void ScoreBoard(int Player, int Computer)
+{
+    if (Player == 1 && Computer == 3 || Player > Computer )
+    {
+        Score1++;
+    }
+    else if ( Player == Computer)
+    {
+        Console.WriteLine("Tie");
+    }
+    else
+    {
+        Score2++;
+    }
+    Console.WriteLine("Rock Paper Scissors Game");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine($"| Player 1: {Score1} | Computer: {Score2} |");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine("What would you like to throw next?");
+    Console.WriteLine("1) Rock");
+    Console.WriteLine("2) Paper");
+    Console.WriteLine("3) Scissors");
+    Console.WriteLine("4) exit");
 }
 
