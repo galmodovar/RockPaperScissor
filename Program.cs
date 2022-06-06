6492c7dea0e1c64b7e0a1023a22f8af3e14e2cf1
galmodovar <galmodovar1@gmail.com>
2022-06-06 11:30:43 -0500

    adding winner and new game function

--- Program.cs
+++ Program.cs
@@ -18,7 +18,6 @@ void Main()
     Console.WriteLine("4) Exit");
     
     Game();
-
 }
 
 int PlayerChoice()
@@ -94,5 +93,68 @@ void ScoreBoard(int Player, int Computer)
     Console.WriteLine("2) Paper");
     Console.WriteLine("3) Scissors");
     Console.WriteLine("4) exit");
+
+    if (Score1 == 3 || Score2 == 3)
+    {
+        EndGame();
+    }
 }
+
+bool NewGame()
+{
+    Console.Write("Start new game? (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write("Start new game? (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void EndGame()
+{
+    if (Score1 == 3)
+    {
+        Console.WriteLine("You Win!");
+        bool isTrue = NewGame();
+        if (isTrue)
+        {
+            Score1 = 0;
+            Score2 = 0;
+            Main();
+        }
+        else
+        {
+            Console.WriteLine("Exiting...");
+            Environment.Exit(0);
+        }
+    }
+    else if (Score2 == 3)
+    {
+         Console.WriteLine("Computer Wins!");
+        bool isTrue = NewGame();
+        if (isTrue)
+        {
+            Score1 = 0;
+            Score2 = 0;
+            Main();
+        }
+        else
+        {
+            Console.WriteLine("Exiting...");
+            Environment.Exit(0);
+        }
+    }   
+    
+
+}
+
 
